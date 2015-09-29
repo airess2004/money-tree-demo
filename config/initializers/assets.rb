8117a9fa220696489437e1f3080ad846342341cd
airess2004 <airess2004@yahoo.com>
2015-09-29 02:36:32 +0000

    Home Page

---
+++ config/initializers/assets.rb
@@ -0,0 +1,39 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w( modernizr.js )
+# Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( theme.js )
+Rails.application.config.assets.precompile += %w( jquery-1.9.1.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui-custom.min.js )
+Rails.application.config.assets.precompile += %w( autoNumeric.min.js )
+Rails.application.config.assets.precompile += %w( jquery.maskedinput.js )
+Rails.application.config.assets.precompile += %w( additional-methods.min.js )
+Rails.application.config.assets.precompile += %w( jquery-cloneya.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui-1.10.4.custom.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui-combo.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui-monthpicker.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui-slider-pips.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui-timepicker.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui-touch-punch.min.js )
+Rails.application.config.assets.precompile += %w( jquery.chained.min.js )
+Rails.application.config.assets.precompile += %w( jquery.form.min.js )
+Rails.application.config.assets.precompile += %w( jquery.formShowHide.min.js )
+Rails.application.config.assets.precompile += %w( jquery.maskedinput.js )
+Rails.application.config.assets.precompile += %w( jquery.placeholder.min.js )
+Rails.application.config.assets.precompile += %w( jquery.spectrum.min.js )
+Rails.application.config.assets.precompile += %w( jquery.stepper.min.js )
+Rails.application.config.assets.precompile += %w( jquery.validate.min.js )
+Rails.application.config.assets.precompile += %w( smart-forms.css )
+Rails.application.config.assets.precompile += %w( black.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( custome.css )
+Rails.application.config.assets.precompile += %w( custom.css )
